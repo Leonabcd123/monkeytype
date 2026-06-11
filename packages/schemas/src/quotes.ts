c8dd08312f048d1083f895b057e5660e6e555f48
Leonabcd123 <156839416+Leonabcd123@users.noreply.github.com>
2026-06-11 15:37:54 +0300

    Rename

--- packages/schemas/src/quotes.ts
+++ packages/schemas/src/quotes.ts
@@ -16,12 +16,12 @@ export const descToGroup = {
   thicc: 3,
 } as const;
 
-export const QuoteLengthKeysSchema = Object.keys(descToGroup) as [
+export const quoteLengthKeys = Object.keys(descToGroup) as [
   keyof typeof descToGroup,
   ...string[],
 ];
 
-export const QuoteLengthSchema = z.enum(QuoteLengthKeysSchema);
+export const QuoteLengthSchema = z.enum(quoteLengthKeys);
 
 export type QuoteLength = z.infer<typeof QuoteLengthSchema>;
 
